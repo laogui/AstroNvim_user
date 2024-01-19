e071cc672dce2fd7411a1cc3d7084754307a5b62
laogui <laogui@gmail.com>
2024-01-19 19:16:53 +0800

    add plugins

--- plugins/user.lua
+++ plugins/user.lua
@@ -1,12 +1,36 @@
 return {
-  -- You can also add new plugins here as well:
-  -- Add plugins, the lazy syntax
-  -- "andweeb/presence.nvim",
-  -- {
-  --   "ray-x/lsp_signature.nvim",
-  --   event = "BufRead",
-  --   config = function()
-  --     require("lsp_signature").setup()
-  --   end,
-  -- },
+    -- You can also add new plugins here as well:
+    -- Add plugins, the lazy syntax
+    -- "andweeb/presence.nvim",
+    -- {
+    --   "ray-x/lsp_signature.nvim",
+    --   event = "BufRead",
+    --   config = function()
+    --     require("lsp_signature").setup()
+    --   end,
+    -- },
+    {
+        "gennaro-tedesco/nvim-possession",
+        dependencies = {
+            "ibhagwan/fzf-lua",
+        },
+        config = true,
+        init = function()
+            local possession = require("nvim-possession")
+            vim.keymap.set("n", "<leader>sl", function()
+                possession.list()
+            end)
+            vim.keymap.set("n", "<leader>sn", function()
+                possession.new()
+            end)
+            vim.keymap.set("n", "<leader>su", function()
+                possession.update()
+            end)
+            vim.keymap.set("n", "<leader>sd", function()
+                possession.delete()
+            end)
+        end,
+    },
+    { "mlaursen/vim-react-snippets", version = false },
+    { "dstein64/vim-startuptime" },
 }
